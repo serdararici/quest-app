d39732cccf661b74f982a98a36b79866a754bf14
Serdar Arici <serdararici3@gmail.com>
2025-07-11 09:16:30 +0300

    Add mappings for Post, Comment, and Like entities to establish table relationships

--- src/main/java/com/example/questapp/entities/Comment.java
+++ src/main/java/com/example/questapp/entities/Comment.java
@@ -1,9 +1,17 @@
 package com.example.questapp.entities;
 
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -14,8 +22,19 @@ public class Comment {
 	
 	@Id
 	Long id;
-	Long postId;
-	Long userId;
+	
+	@ManyToOne(fetch = FetchType.LAZY)   //fetch lazy means we don't need user object when we call posts
+	@JoinColumn(name="post_id", nullable = false)
+	@OnDelete(action = OnDeleteAction.CASCADE)  // when user is deleted, it deletes post that belongs to the user.
+	@JsonIgnore
+	Post post;
+	
+	@ManyToOne(fetch = FetchType.LAZY)   //fetch lazy means we don't need user object when we call posts
+	@JoinColumn(name="user_id", nullable = false)
+	@OnDelete(action = OnDeleteAction.CASCADE)  // when user is deleted, it deletes post that belongs to the user.
+	@JsonIgnore
+	User user;
+	
 	@Lob
 	@Column(columnDefinition = "text")
 	String text;
--- src/main/java/com/example/questapp/entities/Post.java
+++ src/main/java/com/example/questapp/entities/Post.java
@@ -1,9 +1,17 @@
 package com.example.questapp.entities;
 
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -14,7 +22,13 @@ public class Post {
 	
 	@Id
 	Long id;
-	Long userId;
+	
+	@ManyToOne(fetch = FetchType.LAZY)   //fetch lazy means we don't need user object when we call posts
+	@JoinColumn(name="user_id", nullable = false)
+	@OnDelete(action = OnDeleteAction.CASCADE)  // when user is deleted, it deletes post that belongs to the user.
+	@JsonIgnore
+	User user;
+	
 	String title;
 	
 	// Stores a large block of text in the database using the 'TEXT' column type.
